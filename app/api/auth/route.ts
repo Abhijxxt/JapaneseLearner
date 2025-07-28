2ea65737d1fa313dd1e5ad75564987d683dea975
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2025-07-28 21:28:08 +0530

    Created login authorization API

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -3,8 +3,24 @@ import { PrismaClient } from "@/app/generated/prisma/client"
 const prisma = new PrismaClient()
 // use `prisma` in your application to read and write data in your DB
 
-export async function GET(request: NextRequest) {
-    const data = await prisma.user.findMany()
-    console.log(data)
-    return NextResponse.json(data,{status: 200})
+// export async function GET(request: NextRequest) {
+//     const data = await prisma.user.findMany()
+//     // console.log(data)
+//     return NextResponse.json(data,{status: 200})
+// }
+
+export async function POST(request: NextRequest) {
+    const {email, password}= await request.json()
+    console.log(email + " : " + password);
+    const validateUser = await prisma.user.findUnique({
+        where: {
+            email: email,
+            password: password
+        },
+    })
+    if(validateUser) {
+        return NextResponse.json(validateUser,{status: 200})
+    } else {
+        return NextResponse.json(validateUser,{status: 404})
+    }
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,29 @@
 'use client'
 export default function LoginPage() {
+
+    const login = async (event : any) => {
+        event.preventDefault()
+        const response = await fetch("/api/auth", {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: event.target[0].value,
+                password: event.target[1].value
+            })
+        })
+        const data = await response.json()
+        if(response.status === 200) {
+            console.log("True")
+        } else {
+            console.log("False")
+        }
+    }
+
     return(
         <div className="bg-purple-50 h-[90vh] flex flex-col w-screen justify-center items-center">
-            <form className="px-10 py-6">
+            <form className="px-10 py-6" onSubmit={login}>
                 <input type="email" placeholder="abcd@xmy.com" className="p-2 m-2 bg-slate-600 text-white"/><br/>
                 <input type="password" placeholder="********" className="p-2 m-2 bg-slate-600 text-white" /><br/>
                 <input type="submit" value="Submit" className="m-2 p-2 px-4 rounded-md transition-all bg-amber-300 hover:bg-amber-600"/><br/>
