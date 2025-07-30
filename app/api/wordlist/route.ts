08618cd45c4a7058bb6ec665df11bc3400db1cc3
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2025-07-30 23:39:45 +0530

    Installed primsa-extension-random but it didn't work Alternatively used raw query in Prisma to get random order of words

--- app/api/wordlist/route.ts
+++ app/api/wordlist/route.ts
@@ -1,14 +1,16 @@
 import { NextResponse, NextRequest } from "next/server";
 import { prisma } from "@/prisma/client";
+// import prismarandom from 'prisma-extension-random';
 
 export async function POST(request: NextRequest) {
 
 }
 
 export async function GET(request: NextRequest) {
-    const wordlist = await prisma.words.findMany({
-        take: 10
-    }); 
+    // const wordlist = await prisma.words.findMany({
+    //     take: 10
+    // }); 
+    const wordlist = await prisma.$queryRaw`SELECT * FROM words ORDER BY RAND() LIMIT 10`;
     if(!wordlist) {
         return NextResponse.json(wordlist, {status: 400});
     }
