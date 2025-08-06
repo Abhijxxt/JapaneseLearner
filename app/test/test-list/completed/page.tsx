9200c679a200e7f16706c867deaaac8f9b56174d
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2025-08-07 02:53:22 +0530

    Added suspense on completed page as directed on vercel

--- app/test/test-list/completed/page.tsx
+++ app/test/test-list/completed/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { HiViewList } from "react-icons/hi";
 import { IoReload } from "react-icons/io5";
 export default function CompletionPage() {
@@ -39,15 +39,17 @@ export default function CompletionPage() {
     }, [])
 
     return(
-        <div className="w-[100vw] h-[90vh] bg-[url('/complete-test-bg.jpg')] bg-cover bg-no-repeat flex flex-col justify-center items-center">
-            <div className="backdrop-blur-xs bg-white/80 rounded-xl p-8 shadow-lg">
-                <p className="text-3xl font-bold bg-gradient-to-r from-amber-700 to-amber-800 bg-clip-text text-transparent">Correct answers: {score}/{total}</p>
-                <p className="text-center mt-1 text-xl">{printMessage}</p>
-                <div className="flex flex-row justify-center items-center mt-4">
-                    <button onClick={retryButton} className="m-2 flex flex-row justify-center items-center bg-amber-500 hover:bg-amber-700 py-2 px-4 gap-1 rounded-md">Retry<IoReload/></button>
-                    <button onClick={wordButton} className="m-2 flex flex-row justify-center items-center bg-amber-200 hover:bg-amber-600 py-2 px-4 gap-1 rounded-md">Word<HiViewList/></button>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="w-[100vw] h-[90vh] bg-[url('/complete-test-bg.jpg')] bg-cover bg-no-repeat flex flex-col justify-center items-center">
+                <div className="backdrop-blur-xs bg-white/80 rounded-xl p-8 shadow-lg">
+                    <p className="text-3xl font-bold bg-gradient-to-r from-amber-700 to-amber-800 bg-clip-text text-transparent">Correct answers: {score}/{total}</p>
+                    <p className="text-center mt-1 text-xl">{printMessage}</p>
+                    <div className="flex flex-row justify-center items-center mt-4">
+                        <button onClick={retryButton} className="m-2 flex flex-row justify-center items-center bg-amber-500 hover:bg-amber-700 py-2 px-4 gap-1 rounded-md">Retry<IoReload/></button>
+                        <button onClick={wordButton} className="m-2 flex flex-row justify-center items-center bg-amber-200 hover:bg-amber-600 py-2 px-4 gap-1 rounded-md">Word<HiViewList/></button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Suspense>
     )
 }
