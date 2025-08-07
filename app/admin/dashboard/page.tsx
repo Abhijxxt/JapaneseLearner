037d12cec87736b9262da37e630c8ee952b4a1a8
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2025-08-07 16:15:43 +0530

    Admin dashboard button fix Also previously added Schema for admin user

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default function AdminDashboardPage() {
         <div className="bg-amber-50 h-[90vh] flex flex-col w-screen justify-center items-center">
             <h1 className="text-2xl font-bold">Admin Dashboard</h1>
             <p className="mt-4">Welcome to the admin dashboard!</p>
-            <button onClick={BugsReportPage}>Bugs Report</button>
+            <button onClick={BugsReportPage} className="bg-amber-500 px-4 py-2 rounded-md mt-6 transition-all ease-in-out shadow-md hover:shadow-none">Bugs Report</button>
         </div>
     )
 }
