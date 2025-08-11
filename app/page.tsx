a21c462d1070ab20b697065ec38374ee41fb0a94
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2025-08-11 18:57:06 +0530

    Created landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,49 @@
+"use client"
+import { useEffect, useState } from "react";
+
+type Word = {
+  kanji?: string;
+  english: string;
+  japanese: string;
+  romanji: string;
+}
+
 export default function Home() {
+
+  const [word, setWord] = useState<Word>({
+    english: "",
+    japanese: "",
+    romanji: "",
+    kanji: ""
+  })
+
+  const getWord = async () => {
+    const response = await fetch("/api/wordlist")
+    if(response.status === 200) {
+      const data = await response.json();
+      setWord(data[0]);
+    }
+  }
+
+  useEffect(() => {
+    getWord();
+  },[])
+
   return (
-    <div className="w-[100%] h-[90vh] bg-slate-100 flex flex-row justify-center items-center">
-      <div className="p-20 bg-slate-50">
-        <p>外出先で日本語を学ぶ</p>
-        <p>Learn Japanese on the go</p>
-        <button className="bg-amber-600">Get started</button>
+    <div className="w-[100%] h-[90vh] bg-[url('/landing-page-bg.jpg')] bg-cover bg-no-repeat bg-slate-100 flex flex-row justify-center items-center">
+      <div className="mr-10">
+        <p className="text-4xl text-pink-600">外出先で日本語を学ぶ</p>
+        <p className="text-2xl">Learn Japanese on the go</p>
+        <button className="bg-amber-600 px-4 py-2 text-xl mt-4">Get started</button>
       </div>
-      <div>
-        <h1>水</h1>
-        <p>English: Water</p>
-        <p>Furigana: みず</p>
-        <p>Romanji: Mizu</p>
+      <div className="ml-10 p-4 rounded-md shadow-md">
+        <div className="bg-gradient-to-br from-pink-300 to-pink-600 opacity-50"></div>
+        <div>
+          <h1 className="text-3xl ">{word.kanji}</h1>
+          <p className="text-xl">English: {word.english}</p>
+          <p className="text-xl">Furigana: {word.japanese}</p>
+          <p className="text-xl">Romanji: {word.romanji}</p>
+        </div>
       </div>
     </div>
   );
