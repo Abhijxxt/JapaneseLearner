603e3984b5dbdcb02145cfddf694c87712729df2
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2025-08-13 01:06:14 +0530

    Created alert boxes for login signup page, added styling to those as well

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,22 @@
 'use client'
 
 import { redirect } from "next/navigation"
-// import { useRouter } from "next/router"
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+import { AlertCircleIcon } from "lucide-react";
+import { useState } from "react";
 
 export default function LoginPage() {
+
+    const [errorBox , setErrorBox] = useState(false);
+
+    const showErrorBox = () => {
+        setErrorBox(true)
+        setTimeout(() => {setErrorBox(false)}, 2000)
+    }
 
     const login = async (event : any) => {
         event.preventDefault()
@@ -24,17 +37,27 @@ export default function LoginPage() {
             localStorage.setItem('user', JSON.stringify(data));
             redirect("/home")
         } else {
-            console.log("False")
-            alert("Login failed")
+            // console.log("False")
+            // alert("Login failed")
+            showErrorBox()
         }
     }
 
     return(
-        <div className="bg-purple-50 h-[90vh] flex flex-col w-screen justify-center items-center">
-            <form className="px-10 py-6" onSubmit={login}>
-                <input type="email" placeholder="abcd@xmy.com" className="p-2 m-2 bg-slate-600 text-white"/><br/>
-                <input type="password" placeholder="********" className="p-2 m-2 bg-slate-600 text-white" /><br/>
+        <div className="bg-purple-50 bg-[url('/login-signup-bg.jpg')] bg-cover bg-no-repeat h-[90vh] flex flex-col w-screen justify-center items-center">
+            <form className=" bg-slate-100 px-10 py-6 shadow-lg" onSubmit={login}>
+                <input type="email" placeholder="abcd@xmy.com" className="p-2 m-2 rounded-md bg-amber-600 text-black"/><br/>
+                <input type="password" placeholder="********" className="p-2 m-2 rounded-md bg-amber-600 text-black" /><br/>
                 <input type="submit" value="Submit" className="m-2 p-2 px-4 rounded-md transition-all bg-amber-300 hover:bg-amber-600"/><br/>
+                { errorBox &&
+                <Alert variant="destructive" className="">
+                    <AlertCircleIcon />
+                    <AlertTitle>Wrong credentials.</AlertTitle>
+                    <AlertDescription>
+                        Please try again
+                    </AlertDescription>
+                </Alert>
+                }
             </form>
         </div>
     )
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,28 @@
 'use client'
 
 import { redirect } from "next/navigation";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+import { AlertCircleIcon } from "lucide-react";
+import { useState } from "react";
 
 export default function SignupPage() {
+
+    const [errorBox , setErrorBox] = useState(false);
+
+    const showErrorBox = () => {
+        setErrorBox(true)
+        setTimeout(() => {setErrorBox(false)}, 2000)
+    }
 
     const signup = async (event : any) => {
         event.preventDefault()
         if(event.target[3].value !== event.target[4].value) {
-            alert("Password did not match!");
+
+            showErrorBox()
             // event.target[0].value = "";
             // event.target[1].value = "";
             // event.target[2].value = "";
@@ -37,19 +52,37 @@ export default function SignupPage() {
             event.target[4].value = "";
             redirect("/home")
         } else {
-            alert("There was an error signing up. Please check your email or password")
+            showErrorBox()
         }
+            // alert("There was an error signing up. Please check your email or password")
+        
     }
 
     return(
-        <div className="bg-purple-50 h-[90vh] flex flex-col w-screen justify-center items-center">
-            <form className="px-10 py-6 bg-blue-100" onSubmit={signup} >
-                <input type="text" name="" placeholder="First name" id="" className="p-2 m-2 bg-slate-600 text-white"/>
-                <input type="text" name="" placeholder="Last name" id="" className="p-2 m-2 bg-slate-600 text-white"/> <br/>
-                <input type="email" placeholder="abcd@xmy.com" className="p-2 m-2 bg-slate-600 text-white w-full"/><br/>
-                <input type="password" placeholder="********" className="p-2 m-2 bg-slate-600 text-white w-full" /><br/>
-                <input type="password" placeholder="********" className="p-2 m-2 bg-slate-600 text-white w-full" /><br/>
+        <div className="bg-purple-50 bg-[url('/login-signup-bg.jpg')] bg-cover bg-no-repeat h-[90vh] flex flex-col w-screen justify-center items-center">
+            <form className="px-10 py-6 bg-slate-100 shadow-lg" onSubmit={signup} >
+                <div className="w-[100%] flex flex-row justify-center items-center gap-2 m-2">
+                    <input type="text" name="" placeholder="First name" id="" className="w-[50%] p-2 rounded-md bg-amber-600 text-black"/>
+                    <input type="text" name="" placeholder="Last name" id="" className=" w-[50%] p-2 rounded-md  bg-amber-600 text-black"/> <br/>
+                </div>
+                <input type="email" placeholder="abcd@xmy.com" className="p-2 m-2 bg-amber-600 rounded-md text-black w-full"/><br/>
+                <input type="password" placeholder="********" className="p-2 m-2 bg-amber-600 rounded-md text-black w-full" /><br/>
+                <input type="password" placeholder="********" className="p-2 m-2 bg-amber-600 rounded-md text-black w-full" /><br/>
                 <input type="submit" value="Submit" className="m-2 p-2 px-4 rounded-md transition-all bg-amber-300 hover:bg-amber-600"/><br/>
+                { errorBox &&
+                <Alert variant="destructive" className="w-full">
+                    <AlertCircleIcon />
+                    <AlertTitle>Unable to signup user with current Email.</AlertTitle>
+                    <AlertDescription>
+                        <p>Please verify your email and password information and try again.</p>
+                        <ul className="list-inside list-disc text-sm">
+                            <li>Check if you already have an account</li>
+                            <li>Check for correct email address</li>
+                            <li>Check for correct password</li>
+                        </ul>
+                    </AlertDescription>
+                </Alert>
+                }
             </form>
         </div>
     )
